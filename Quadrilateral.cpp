fea3d50ae6b5071d31529cb02054ca9071c14456
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-05-31 15:14:33 +0700

    Update Quadrilateral.cpp

--- Quadrilateral.cpp
+++ Quadrilateral.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 Quadrilateral::Quadrilateral(int a, int b, int c, int d, int A, int B, int C, int D)
-    : Figure("Четырёхугольник"), a(a), b(b), c(c), d(d), A(A), B(B), C(C), D(D) {
+    : Figure("Р§РµС‚С‹СЂС‘С…СѓРіРѕР»СЊРЅРёРє"), a(a), b(b), c(c), d(d), A(A), B(B), C(C), D(D) {
 }
 
 bool Quadrilateral::check() const {
@@ -11,6 +11,6 @@ bool Quadrilateral::check() const {
 
 void Quadrilateral::print_info() const {
     Figure::print_info();
-    std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-    std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+    std::cout << "РЎС‚РѕСЂРѕРЅС‹: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
+    std::cout << "РЈРіР»С‹: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
 }
